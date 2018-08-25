905a7791a03ad7e27eb3dc19ef87e483fccb3657
Garet Eliot <self@garet.io>
2018-08-25 13:57:28 -0400

    use react-ga for analytics instead of script embed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import ReactGA from "react-ga";
 import "animate.css/animate.min.css";
 
 import "./global";
@@ -9,6 +10,11 @@ import Intro from "./sections/Intro";
 import History from "./sections/History";
 import About from "./sections/About";
 import Contact from "./sections/Contact";
+
+if (process.env.NODE_ENV === "production") {
+  ReactGA.initialize("UA-124514795-1");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 const App = () => (
   <ThemeProvider theme={theme}>
